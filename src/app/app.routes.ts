f3da7510fcdda0a8cfc0764dcc2ec47e0d5e67d8
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-03-29 20:40:10 -0300

    feat: set routes base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './pages/not-logged/register/register.component';
+import { HomeComponent } from './pages/logged/home/home.component';
+import { LoginComponent } from './pages/not-logged/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    }
+];
