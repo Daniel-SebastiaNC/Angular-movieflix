7be8cee6780462c24a85dfd6aa33a3646657748b
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-05-05 17:10:43 -0300

    feat: add route to /register-movie

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,16 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './pages/not-logged/register/register.component';
 import { HomeComponent } from './pages/logged/home/home.component';
 import { LoginComponent } from './pages/not-logged/login/login.component';
+import { RegisterMovieComponent } from './pages/logged/register-movie/register-movie.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent
+    },
+    {
+        path: 'register-movie',
+        component: RegisterMovieComponent
     },
     {
         path: 'login',
